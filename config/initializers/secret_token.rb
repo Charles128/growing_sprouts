bcdf6e40358db6b0064b3386bd4ba6a51aee6750
Charles128 <charles.amoako.128@gmail.com>
2013-12-20 00:55:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowingSprouts::Application.config.secret_token = '113985508048bc4e818459d17fd589fa46783ce111f76cbd822e38ef06ee61059fb20ca2da9d3cf8a64161117d935455497aab5a2851e1a9271fd72228466f64'
