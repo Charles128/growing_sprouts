ceab34228e86efeffac3e35c70e41bcd5229be1f
Charles128 <charles.amoako.128@gmail.com>
2014-02-08 17:47:05 -0500

    added HTML5 validations to contact us form and confirmation message when complete

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,10 +1,5 @@
 class EmailsController < ApplicationController
   def contact_us
-    # ContactUsMailer.contact_us_email(params).deliver
-    if ContactUsMailer.contact_us_email(params).deliver
-      render text: "success"
-    else
-      render text: "fail"
-    end
+    ContactUsMailer.contact_us_email(params).deliver
   end
 end
